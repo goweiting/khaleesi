875fcb6999f3b24ae48041946890918699c06593
goweiting <gwt.543@gmail.com>
2017-01-31 21:43:56 +0000

    we should really wor on our own branch ... STOP ROLLING BACK MY COMMITS

--- sdpJava/src/strategy/drives/RobotKinematic.java
+++ sdpJava/src/strategy/drives/RobotKinematic.java
@@ -1,10 +1,10 @@
+package strategy.drives;
 
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.factory.LinearSolverFactory;
 import org.ejml.interfaces.linsol.LinearSolver;
 import vision.tools.DirectedPoint;
 import vision.tools.VectorGeometry;
-
 
 /**
  * Created by goweiting on 31/01/17.
@@ -16,45 +16,83 @@ public class RobotKinematic {
 
   // The kicker of the robot is pointing towards the positive y-axis of the field
   // A point P is chosen wrt to the centre of the robot
-  private final double frontRight = pi / 4; // these are the angles of the wheels wrt to point P
-  private final double frontLeft = 5 * pi / 8;
-  private final double backWheel = -1 * pi / 2;
-  private final double distL = 1; // assume distance between wheels and P is 1
-  private final double radius = 1; // assume that the radius of the wheels is r
-  private final double gamma = 0; // the roller are 90-degrees to the wheel
-  private final double beta = 0; // because the wheels are tangent to the robot's circular body
+  private final double frontRight = pi / 6;
+  private final double frontLeft = 5 * pi / 6;
+  private final double backWheel = 9 * pi / 6;
 
-  public FixedMatrix3x3_64F epsilon_I = new FixedMatrix3x3_64F();
-  public FixedMatrix3x3_64F J_2 = new FixedMatrix3x3_64F(
-      radius, 0, 0,
-      0, radius, 0,
-      0, 0, radius
-  ); // this is basically a diagonal with r
-  public FixedMatrix3x3_64F J_1f = new FixedMatrix3x3_64F(
-      Math.sin(frontLeft), -1 * Math.cos(frontLeft), -1 * distL,
-      0, -1 * Math.cos(frontRight), -1 * distL,
-      Math.sin(backWheel), -1 * Math.cos(backWheel), -1 * distL
+  private DenseMatrix64F m = new DenseMatrix64F(3, 3, true,
+      Math.cos(frontLeft + pi / 2), Math.cos(frontRight + pi / 2), Math.cos(backWheel + pi / 2),
+      Math.sin(frontLeft + pi / 2), Math.sin(frontRight + pi / 2), Math.sin(backWheel + pi / 2),
+      1, 0, 1
   );
 
+//  public FixedMatrix3x3_64F J_2 = new FixedMatrix3x3_64F(
+//      radius, 0, 0,
+//      0, radius, 0,
+//      0, 0, radius
+//  ); // this is basically a diagonal with r
+
+//  public FixedMatrix3x3_64F J_1f = new FixedMatrix3x3_64F(
+//      Math.sin(frontLeft), -1 * Math.cos(frontLeft), -1 * distL,
+//      0, -1 * Math.cos(frontRight), -1 * distL,
+//      Math.sin(backWheel), -1 * Math.cos(backWheel), -1 * distL
+//  );
+
+
+  public RobotKinematic() {
+  }
+
   /**
-   * Using the forward kinematic model, computes the speed for each wheels;
+   * Apply the inverse kinematic model to derieved the speed required to drive the robot
+   * from current location to the desired vector v = (force, rotation).T
    *
-   * @param location Current location of the robot (x,y,theta)
-   * @param force The desired location
-   * @param rotation The desired angle
-   * Returns the speeds for each of the motors
+   * @param location the current location of the robot
+   * @param force the desired location in (x,y)
+   * @param rotation difference between the the heading of the robot and the desired location
    */
-  public double[] forwardKinematic(
-      DirectedPoint location, VectorGeometry force, double rotation) {
-    double theta = location.getDirection();// current location
-//    FixedMatrix3x3_64F orthogonalRotation = new FixedMatrix3x3_64F(
-//        Math.cos(theta), Math.sin(theta), 0,
-//        -1 * Math.sin(theta), Math.cos(theta), 0,
-//        0, 0, 1
-//    );
+  public int[] inverseKinematic(DirectedPoint location, VectorGeometry force,
+      double rotation) {
+
+    // The desired vector to return (the speed of each wheels)
+    double new_x = force.getX() - location.getX();
+    double new_y = force.getY() - location.getY();
+    double theta = rotation - location.getDirection();
+    DenseMatrix64F v = new DenseMatrix64F(3, 1, true,
+        new_x, new_y, theta);
+    DenseMatrix64F s = new DenseMatrix64F(3, 1);
+    // Calculate the inverse of the designed matrix
+    LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.qr(3, 3);
+    if (solver.setA(m)) {
+      solver.solve(v, s);
+    }
+
+    int[] wheelSpeeds = {
+        (int) s.get(0) * 100,
+        (int) s.get(1) * 100,
+        (int) s.get(2) * 100
+    };
+
+    return (checkValue(wheelSpeeds));
+  }
 
 
+  private static int[]checkValue(int[] wheelSpeeds) {
+    int maxV = getMaxValue(wheelSpeeds);
+    for (int i:wheelSpeeds){
+      wheelSpeeds[i] = wheelSpeeds[i]/maxV * 100;
+    }
+    return wheelSpeeds;
+  }
 
+  private static int getMaxValue(int[] array) {
+    int maxValue = array[0];
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] > maxValue) {
+        maxValue = array[i];
+      }
+    }
+    return maxValue;
   }
 
 }
+
