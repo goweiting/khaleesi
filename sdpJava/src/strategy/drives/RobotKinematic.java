d670e86c208a080ea4b7641fc77b2937f6b5161b
goweiting <gwt.543@gmail.com>
2017-01-31 20:44:47 +0000

    Using relative distance for  velocity matrix!

--- sdpJava/src/strategy/drives/RobotKinematic.java
+++ sdpJava/src/strategy/drives/RobotKinematic.java
@@ -1,7 +1,7 @@
 package strategy.drives;
 
-import org.ejml.factory.LinearSolverFactory;
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
 import org.ejml.interfaces.linsol.LinearSolver;
 import vision.tools.DirectedPoint;
 import vision.tools.VectorGeometry;
@@ -54,20 +54,29 @@ public class RobotKinematic {
    * @param force the desired location in (x,y)
    * @param rotation difference between the the heading of the robot and the desired location
    */
-  public DenseMatrix64F inverseKinematic(DirectedPoint location, VectorGeometry force,
+  public int[] inverseKinematic(DirectedPoint location, VectorGeometry force,
       double rotation) {
 
     // The desired vector to return (the speed of each wheels)
+    double new_x = force.getX() - location.getX();
+    double new_y = force.getY() - location.getY();
+    double theta = rotation - location.getDirection();
     DenseMatrix64F v = new DenseMatrix64F(3, 1, true,
-        force.getX(), force.getY(), rotation);
+        new_x, new_y, theta);
     DenseMatrix64F s = new DenseMatrix64F(3, 1);
     // Calculate the inverse of the designed matrix
-    LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.qr(3,3);
+    LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.qr(3, 3);
     if (solver.setA(m)) {
       solver.solve(v, s);
     }
-//    System.out.print(s.toString());
-    return (s);
+
+    int[] wheelSpeeds = {
+        (int) s.get(0) * 100,
+        (int) s.get(1) * 100,
+        (int) s.get(2) * 100
+    };
+
+    return (wheelSpeeds);
   }
 
 }
--- sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
+++ sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
@@ -2,7 +2,6 @@ package strategy.drives;
 
 import communication.ports.interfaces.RobotPort;
 import communication.ports.interfaces.ThreeWheelHolonomicRobotPort;
-import org.ejml.data.DenseMatrix64F;
 import vision.tools.DirectedPoint;
 import vision.tools.VectorGeometry;
 
@@ -15,7 +14,8 @@ public class ThreeWheelHolonomicDrive implements DriveInterface {
 
   public int MAX_ROTATION = 55;
   public int MAX_MOTION = 100;
-    private final double MIN_MOTOR = 60;
+  private final double MIN_MOTOR = 60;
+
   /**
    * Moving the three wheel holonomic robot to a desired location.
    *
@@ -28,20 +28,16 @@ public class ThreeWheelHolonomicDrive implements DriveInterface {
    * @param factor scaling factor for todo:idk what!
    */
   public void move(
-      RobotPort port, DirectedPoint location, VectorGeometry force, double rotation, double factor) {
+      RobotPort port, DirectedPoint location, VectorGeometry force, double rotation,
+      double factor) {
     assert (port instanceof ThreeWheelHolonomicRobotPort);
 
     RobotKinematic kinematics = new RobotKinematic();
-    DenseMatrix64F v = kinematics.inverseKinematic(location, force, rotation);
-
-    double frontLeft, frontRight, backWheel;
-      frontLeft = v.get(0) * 100;
-      frontRight = (v.get(1) * 100);
-      backWheel = (v.get(2) * 100);
-
+    int[] wheelSpeeds = kinematics.inverseKinematic(location, force, rotation);
 
     // Instructs the robot to to the desired location with that amount of "speed"
-    ((ThreeWheelHolonomicRobotPort)port).threeWheelHolonomicMotion(frontLeft, frontRight, backWheel);
+    ((ThreeWheelHolonomicRobotPort) port)
+        .threeWheelHolonomicMotion(wheelSpeeds[0], wheelSpeeds[1], wheelSpeeds[2]);
   }
 
 }
