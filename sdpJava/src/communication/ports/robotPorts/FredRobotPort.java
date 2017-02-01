51f60ca32b1b1eba16793dc7ee33c674f2062553
verden11 <justinas.reigis@gmail.com>
2017-02-01 02:10:57 +0000

    minor changes

--- sdpJava/src/communication/ports/robotPorts/FredRobotPort.java
+++ sdpJava/src/communication/ports/robotPorts/FredRobotPort.java
@@ -9,38 +9,38 @@ import communication.ports.interfaces.ThreeWheelHolonomicRobotPort;
  * Created by Simon Rovder
  */
 public class FredRobotPort extends RobotPort implements
-    PropellerEquipedRobotPort, ThreeWheelHolonomicRobotPort, DribblerKickerEquippedRobotPort {
+        PropellerEquipedRobotPort, ThreeWheelHolonomicRobotPort, DribblerKickerEquippedRobotPort {
 
-  public FredRobotPort() {
-    super("pang");
-  }
+    public FredRobotPort() {
+        super("pang");
+    }
 
-  @Override
-  public void threeWheelHolonomicMotion(double frontLeft, double frontRight, double back) {
-    // for debugging
-    System.out.printf("frontleft : %d\nfrontRight : %d\nback: %d", frontLeft, frontRight, back);
-    this.sdpPort.commandSender("r", (int) frontLeft, (int) frontRight, (int) back);
-  }
+    @Override
+    public void threeWheelHolonomicMotion(double frontLeft, double frontRight, double back) {
+        // for debugging
+        System.out.printf("frontleft : " + frontLeft + " \nfrontRight : " + frontRight + "\nback: " + back + "\n");
+        this.sdpPort.commandSender("r", (int) frontLeft, (int) frontRight, (int) back);
+    }
 
-  // command to spin dribbler and kicker
-  public void dribblerKicker(double dribbler, double kickerL, double kickerR) {
-    this.sdpPort.commandSender("dk", (int) dribbler, (int) kickerL, (int) kickerR);
-  }
+    // command to spin dribbler and kicker
+    public void dribblerKicker(double dribbler, double kickerL, double kickerR) {
+        this.sdpPort.commandSender("dk", (int) dribbler, (int) kickerL, (int) kickerR);
+    }
 
-  // command to spin dribbler and kicker
-  @Override
-  public void dribblerKicker(double dribbler, double kicker) {
-    this.sdpPort.commandSender("dk", (int) dribbler, (int) kicker);
-  }
+    // command to spin dribbler and kicker
+    @Override
+    public void dribblerKicker(double dribbler, double kicker) {
+        this.sdpPort.commandSender("dk", (int) dribbler, (int) kicker);
+    }
 
-  public void kicker(double kickerL, double kickerR) {
-    this.sdpPort.commandSender("kicker", (int) kickerL, (int) kickerR);
-  }
+    public void kicker(double kickerL, double kickerR) {
+        this.sdpPort.commandSender("kicker", (int) kickerL, (int) kickerR);
+    }
 
-//   will keep it for reference, there is some extra login in arduino code for this
+    //   will keep it for reference, there is some extra login in arduino code for this
     @Override
     public void propeller(int spin) {
-      this.sdpPort.commandSender("kick", spin);
+        this.sdpPort.commandSender("kick", spin);
     }
 
 
--- sdpJava/src/strategy/drives/RobotKinematic.java
+++ sdpJava/src/strategy/drives/RobotKinematic.java
@@ -9,64 +9,70 @@ import vision.tools.VectorGeometry;
  */
 public class RobotKinematic {
 
-  private final double pi = Math.PI; // shorthand
+    private final double pi = Math.PI; // shorthand
 
-  // The kicker of the robot is pointing towards the positive y-axis of the field
-  // A point P is chosen wrt to the centre of the robot
-  private final double frontRight = pi / 6;
-  private final double frontLeft = 5 * pi / 6;
-  private final double backWheel = 9 * pi / 6;
+    // The kicker of the robot is pointing towards the positive y-axis of the field
+    // A point P is chosen wrt to the centre of the robot
+    private final double frontRight = pi / 6;
+    private final double frontLeft = 5 * pi / 6;
+    private final double backWheel = 9 * pi / 6;
 
-//  private DenseMatrix64F m = new DenseMatrix64F(3, 3, true,
-//      Math.cos(frontLeft + pi / 2), Math.cos(frontRight + pi / 2), Math.cos(backWheel + pi / 2),
-//      Math.sin(frontLeft + pi / 2), Math.sin(frontRight + pi / 2), Math.sin(backWheel + pi / 2),
-//      1, 0, 1
-//  );
 
-  public RobotKinematic() {
-  }
-
-  /**
-   * Apply the inverse kinematic model to derieved the speed required to drive the robot
-   * from current location to the desired vector v = (force, rotation).T
-   *
-   * @param location the current location of the robot
-   * @param force the desired location in (x,y)
-   * @param rotation difference between the the heading of the robot and the desired location
-   */
-  public double[] inverseKinematic(DirectedPoint location, VectorGeometry force,
-      double rotation) {
-
-    // The desired vector to return (the speed of each wheels)
-    double new_x = force.getX() - location.getX();
-    double new_y = force.getY() - location.getY();
-    double theta = rotation - location.getDirection();
-
-    double[] wheelSpeeds = new double[3];
-    wheelSpeeds[0] = 0.5 * new_x + (-1 * (Math.sqrt(3) / 2) * new_y) + (-1 * theta);
-    wheelSpeeds[1] = 0.5 * new_x + (Math.sqrt(3) / 2) * new_y + (-1 * theta);
-    wheelSpeeds[2] = -1 * new_x + 0 + (-1 * theta);
-
-    return wheelSpeeds;
-  }
-
-  private static int[] checkValue(int[] wheelSpeeds) {
-    int maxV = getMaxValue(wheelSpeeds);
-    for (int i : wheelSpeeds) {
-      wheelSpeeds[i] = wheelSpeeds[i] / maxV * 100;
+    public RobotKinematic() {
     }
-    return wheelSpeeds;
-  }
 
-  private static int getMaxValue(int[] array) {
-    int maxValue = array[0];
-    for (int i = 1; i < array.length; i++) {
-      if (array[i] > maxValue) {
-        maxValue = array[i];
-      }
+    /**
+     * Apply the inverse kinematic model to derieved the speed required to drive the robot
+     * from current location to the desired vector v = (force, rotation).T
+     *
+     * @param location the current location of the robot
+     * @param force    the desired location in (x,y)
+     * @param rotation difference between the the heading of the robot and the desired location
+     */
+    public double[] inverseKinematic(DirectedPoint location, VectorGeometry force,
+                                     double rotation) {
+
+        // The desired vector to return (the speed of each wheels)
+        double new_x = force.getX() - location.getX();
+        double new_y = force.getY() - location.getY();
+        double theta = rotation - location.getDirection();
+
+        double[] wheelSpeeds = new double[3];
+        wheelSpeeds[0] = (0.5 * new_x + ((Math.sqrt(3) / 2) * new_y) + (-1 * theta));
+        wheelSpeeds[1] = -1 * (0.5 * new_x + (-1 * (Math.sqrt(3) / 2) * new_y) + (-1 * theta));
+        wheelSpeeds[2] = (-1 * new_x) + (-1 * theta);
+
+        return checkValue(wheelSpeeds);
     }
-    return maxValue;
-  }
+
+    private static double[] checkValue(double[] wheelSpeeds) {
+        double maxV = getMaxValue(wheelSpeeds);
+        if (maxV == 0) {
+            return new double[]{0.0, 0.0, 0.0};
+        } else {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                double tmp = wheelSpeeds[i] / maxV * 100;
+                if (tmp > 100) {
+                    wheelSpeeds[i] = 100;
+                } else if (tmp < -100) {
+                    wheelSpeeds[i] = -100;
+                } else {
+                    wheelSpeeds[i] = tmp;
+                }
+            }
+        }
+        return wheelSpeeds;
+    }
+
+    private static double getMaxValue(double[] array) {
+        double maxValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (Math.abs(array[i]) > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
 
 }
 
