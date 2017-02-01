b059dc379bb40f94153dcce8f817fba7ea29ee89
goweiting <gwt.543@gmail.com>
2017-02-01 20:38:29 +0000

    merge RobotKinematic and HolonomicDrive into one file

--- sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
+++ sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
@@ -2,7 +2,6 @@ package strategy.drives;
 
 import communication.ports.interfaces.RobotPort;
 import communication.ports.interfaces.ThreeWheelHolonomicRobotPort;
-import org.ejml.data.DenseMatrix64F;
 import vision.tools.DirectedPoint;
 import vision.tools.VectorGeometry;
 
@@ -28,15 +27,77 @@ public class ThreeWheelHolonomicDrive implements DriveInterface {
    * @param factor scaling factor for todo:idk what!
    */
   public void move(
-      RobotPort port, DirectedPoint location, VectorGeometry force, double rotation, double
-      factor) {
+      RobotPort port, DirectedPoint location, VectorGeometry force, double rotation,
+      double factor) {
     assert (port instanceof ThreeWheelHolonomicRobotPort);
 
-    RobotKinematic kinematics = new RobotKinematic();
-    DenseMatrix64F v = kinematics.inverseKinematic(location, force, rotation);
+    // Some debug print out:
+    System.out.println("My current location" + location.toString());
+    System.out.println("My desired location" + force.toString() + " at angle of :" + rotation);
 
     // Instructs the robot to to the desired location with that amount of "speed"
-    ((ThreeWheelHolonomicRobotPort) port).threeWheelHolonomicMotion(v.get(0), v.get(1), v.get(2));
+    ((ThreeWheelHolonomicRobotPort) port).threeWheelHolonomicMotion();
   }
+
+  /**
+   * Apply the inverse kinematic model to derieved the speed required to drive the robot
+   * from current location to the desired vector v = (force, rotation).T
+   *
+   * @param location the current location of the robot
+   * @param force the desired location in (x,y)
+   * @param rotation difference between the the heading of the robot and the desired location
+   */
+  public double[] inverseKinematic(
+      DirectedPoint location, VectorGeometry force, double rotation) {
+
+    // The desired vector to return (the speed of each wheels)
+    double new_x = force.getX() - location.getX();
+    double new_y = force.getY() - location.getY();
+    double theta = rotation - location.getDirection();
+
+    // Check that theta does not exceed 180degrees (pi), because there's always a simpler way
+    // to get there if that happens
+
+    double[] wheelSpeeds = new double[3];
+    // its frontLeft, frontRight then backWheel!!
+    wheelSpeeds[0] = (-0.3333) * new_x + (-.5774) * new_y + .3333 * theta;
+    wheelSpeeds[1] = (-0.3333) * new_x + (.5774) * new_y + .3333 * theta;
+    wheelSpeeds[2] = (0.6667) * new_x + 0 + 0.3333 * theta;
+
+    // Added Debug:
+    System.out
+        .printf("FL: " + wheelSpeeds[0] + " FR: " + wheelSpeeds[1] + "Back: " + wheelSpeeds[2]);
+    return checkValue(wheelSpeeds);
+  }
+
+  private static double[] checkValue(double[] wheelSpeeds) {
+    double maxV = getMaxValue(wheelSpeeds);
+    if (maxV == 0) {
+      return new double[]{0.0, 0.0, 0.0};
+    } else {
+      for (int i = 0; i < wheelSpeeds.length; i++) {
+        double tmp = wheelSpeeds[i] / maxV * 100;
+        if (tmp > 100) {
+          wheelSpeeds[i] = 100;
+        } else if (tmp < -100) {
+          wheelSpeeds[i] = -100;
+        } else {
+          wheelSpeeds[i] = tmp;
+        }
+      }
+    }
+    return wheelSpeeds;
+  }
+
+  private static double getMaxValue(double[] array) {
+    double maxValue = array[0];
+    for (int i = 1; i < array.length; i++) {
+      if (Math.abs(array[i]) > maxValue) {
+        maxValue = array[i];
+      }
+    }
+    return maxValue;
+  }
+
 
 }
