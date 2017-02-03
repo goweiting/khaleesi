623510c02813aca75970ddcec51c51e5dbdd7932
verden11 <justinas.reigis@gmail.com>
2017-02-03 12:06:00 +0000

    merged three wheel drive into master

--- sdpJava/src/strategy/controllers/essentials/ActionController.java
+++ sdpJava/src/strategy/controllers/essentials/ActionController.java
@@ -15,49 +15,48 @@ public class ActionController extends ControllerBase {
     public DirectedPoint location;
     public ActionInterface action;
 
-    public ActionController(RobotBase robot){
+    public ActionController(RobotBase robot) {
         super(robot);
         this.location = new DirectedPoint(0, 0, 0);
 
     }
 
-    public void setLocation(DirectedPoint location){
-        if(location != null) this.location = location;
+    public void setLocation(DirectedPoint location) {
+        if (location != null) this.location = location;
     }
 
-    public void setAction(ActionInterface action){
-        if(action != null){
+    public void setAction(ActionInterface action) {
+        if (action != null) {
             GUI.gui.action.setText(action.getClass().getName());
         }
 
-        this.action  = action;
+        this.action = action;
     }
 
-    public void commandWait(){
+    public void commandWait() {
         this.action = new Waiting(this.robot);
     }
 
-    public void commandContemplate(){
+    public void commandContemplate() {
         this.action = new Contemplating(null);
     }
 
-    public void perform(){
-        if(this.action == null) return;
+    public void perform() {
+        if (this.action == null) return;
         try {
             this.action.tik();
         } catch (ActionException e) {
-            if(e.getSuccess()){
-                if(!e.getContinueOnExit()) this.robot.port.stop();
+            if (e.getSuccess()) {
+                if (!e.getContinueOnExit()) this.robot.port.stop();
                 this.commandWait();
-            }
-            else{
-                if(!e.getContinueOnExit()) this.robot.port.stop();
+            } else {
+                if (!e.getContinueOnExit()) this.robot.port.stop();
                 this.commandContemplate();
             }
         }
     }
 
-    public void printDescription(){
+    public void printDescription() {
         System.out.println(this.robot.robotType.toString() + this.action.description());
     }
 }
--- sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
+++ sdpJava/src/strategy/drives/ThreeWheelHolonomicDrive.java
@@ -12,36 +12,36 @@ import vision.tools.VectorGeometry;
 
 public class ThreeWheelHolonomicDrive implements DriveInterface {
 
-  public int MAX_ROTATION = 55;
-  public int MAX_MOTION = 100;
-  private final double MIN_MOTOR = 60;
+    public int MAX_ROTATION = 55;
+    public int MAX_MOTION = 100;
+    private final double MIN_MOTOR = 60;
 
-  /**
-   * Moving the three wheel holonomic robot to a desired location.
-   *
-   * @param port interface to command the robot
-   * @param location Current location of the robot - the origin (x,y) = (0,0) of the field is the
-   * centre of the entire field
-   * @param force The direction that the robot should move at the current timeslice to reached the
-   * desired direction
-   * @param rotation The rotation required to achieve the goal?
-   * @param factor scaling factor for todo:idk what!
-   */
-  public void move(
-      RobotPort port, DirectedPoint location, VectorGeometry force, double rotation,
-      double factor) {
-    assert (port instanceof ThreeWheelHolonomicRobotPort);
+    /**
+     * Moving the three wheel holonomic robot to a desired location.
+     *
+     * @param port     interface to command the robot
+     * @param location Current location of the robot - the origin (x,y) = (0,0) of the field is the
+     *                 centre of the entire field
+     * @param force    The direction that the robot should move at the current timeslice to reached the
+     *                 desired direction
+     * @param rotation The rotation required to achieve the goal?
+     * @param factor   scaling factor for todo:idk what!
+     */
+    public void move(
+            RobotPort port, DirectedPoint location, VectorGeometry force, double rotation,
+            double factor) {
+        assert (port instanceof ThreeWheelHolonomicRobotPort);
 
-    // Some debug print out:
-    System.out.println("My current location" + location.toString());
-    System.out.println("My desired location" + force.toString() + " at angle of :" + rotation);
+        // Some debug print out:
+        System.out.println("My current location" + location.toString());
+        System.out.println("My desired location" + force.toString() + " at angle of :" + rotation);
 
-    RobotKinematic kinematics = new RobotKinematic();
-    double[] wheelSpeeds = kinematics.inverseKinematic(location, force, rotation);
+        RobotKinematic kinematics = new RobotKinematic();
+        double[] wheelSpeeds = kinematics.inverseKinematic(location, force, rotation);
 
-    // Instructs the robot to to the desired location with that amount of "speed"
-    ((ThreeWheelHolonomicRobotPort) port)
-        .threeWheelHolonomicMotion(wheelSpeeds[0], wheelSpeeds[1], wheelSpeeds[2]);
-  }
+        // Instructs the robot to to the desired location with that amount of "speed"
+        ((ThreeWheelHolonomicRobotPort) port)
+                .threeWheelHolonomicMotion(wheelSpeeds[0], wheelSpeeds[1], wheelSpeeds[2]);
+    }
 
 }
