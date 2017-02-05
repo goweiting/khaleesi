e8d9ee75c6db8d2bc3121d2ff53bed053b621933
goweiting <gwt.543@gmail.com>
2017-02-05 12:55:43 +0000

    reformatted and reorganised firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,7 +10,7 @@
 #define BACK 4
 
 // Kickers
-#define DRIBLER 0
+#define DRIBBLER 0
 #define KICKERS 1
 #define KICKERS2 2
 
@@ -36,156 +36,165 @@ int run = 0;
 
 SerialCommand sCmd;
 
-void muxTest(){
-    int motor = atoi(sCmd.next());
-    int dir  = atoi(sCmd.next());
-    int pow  = atoi(sCmd.next());
-    Wire.beginTransmission(OPADDR);
-    Wire.write(motor);
-    Wire.write(dir);
-    Serial.println(Wire.endTransmission());
-    Wire.beginTransmission(OPADDR);
-    Wire.write(motor+1);
-    Wire.write(pow);
-    Serial.println(Wire.endTransmission());
-    delay(2000);
-    Wire.beginTransmission(OPADDR);
-    Wire.write(motor);
-    Wire.write(0);
-    Serial.println(Wire.endTransmission());
+
+// =====================================================
+void setup(){
+        Wire.begin();
+        sCmd.addCommand("f", dontMove);
+        sCmd.addCommand("h", completeHalt);
+        sCmd.addCommand("r", rationalMotors);
+        sCmd.addCommand("dk", dribblerKick);
+        sCmd.addCommand("ping", pingMethod);
+        sCmd.addCommand("kick", kicker);
+        sCmd.addCommand("mux", muxTest);
+        SDPsetup();
 }
 
-
 void loop(){
-    sCmd.readSerial();
+        sCmd.readSerial();
+}
+
+void pingMethod(){
+        Serial.println("pang");
+}
+
+void completeHalt(){
+        motorAllStop();
+}
+
+//  ====================================
+//      WHEELS
+//  ====================================
+
+void dontMove(){
+        // stop the three wheels
+        motorStop(FRONTLEFT);
+        motorStop(BACK);
+        motorStop(FRONTRIGHT);
 }
 
 void moveMotor(int motor, int power) {
-    if (power == 0) {
-        motorStop(motor);
-    }
-    else if (power > 0) {
-        motorForward(motor, power);
-    }
-    else {
-      motorBackward(motor, -power);
-    }
+        if (power == 0) { motorStop(motor); }
+        else if (power > 0) { motorForward(motor, power); }
+        else { motorBackward(motor, -power); }
 }
 
-
-void dontMove(){
-    // stop all action
-    motorControl(FRONTLEFT, 0);
-    motorControl(BACK, 0);
-    motorControl(FRONTRIGHT, 0);
-    motorControl(DRIBLER, 0);
-    motorControl(KICKERS, 0);
-    motorControl(KICKERS2, 0);
-}
-
-void spinmotor(){
-    // for testing the motor
-    int motor = atoi(sCmd.next());
-    int power = atoi(sCmd.next());
-    motorForward(motor, power);
-}
-
-void motorControl(int motor, int power){
-    if(power == 0) {
-        Wire.beginTransmission(OPADDR);
-        Wire.write(motor);
-        Wire.write(0);
-        Wire.endTransmission();
-    } else if(power > 0) {
-        Wire.beginTransmission(OPADDR);
-        Wire.write(motor);
-        Wire.write(1);
-        Wire.endTransmission();
-        Wire.beginTransmission(OPADDR);
-        Wire.write(motor + 1);
-        Wire.write(power);
-        Wire.endTransmission();
-    } else {
-        Wire.beginTransmission(OPADDR);
-        Wire.write(motor);
-        Wire.write(2);
-        Wire.endTransmission();
-        Wire.beginTransmission(OPADDR);
-        Wire.write(motor + 1);
-        Wire.write(-power); // note change of sign
-        Wire.endTransmission();
-    }
-}
-
-
-void dribblerKick(){
-    int dribbler = atoi(sCmd.next());
-    int kickPower = atoi(sCmd.next());
-    moveMotor(DRIBLER, dribbler);
-    moveMotor(KICKERS, kickPower);
-    moveMotor(KICKERS2, -kickPower);
-}
 
 void rationalMotors(){
-    // positive power causes the mototr to go clockwise
-    int frontLeft = atoi(sCmd.next());
-    int frontRight = atoi(sCmd.next());
-    int back  = atoi(sCmd.next());
+        // positive power causes the motor to go COUNTER clockwise
+        // note that FRONTLEFT comes first
+        int frontLeft = atoi(sCmd.next());
+        int frontRight = atoi(sCmd.next());
+        int back  = atoi(sCmd.next());
 
-    // changed the polarity here. software should be idiot proof then
-    moveMotor(FRONTLEFT, frontLeft);
-    moveMotor(FRONTRIGHT, -frontRight);
-    moveMotor(BACK, back);
+        // changed the polarity here. software should be idiot proof then
+        moveMotor(FRONTLEFT, frontLeft);
+        moveMotor(FRONTRIGHT, -frontRight);
+        moveMotor(BACK, back);
+}
+
+
+//  ====================================
+//      DRIBBLER AND KICKERS
+//  ====================================
+
+void stopKicker(){
+        motorStop(KICKER);
+}
+
+void stopDribbler(){
+    motorStop(DRIBBLER);
+}
+
+void dribblerKick(){
+        // first one is the power, second one is the power for kicker
+        int dribbler = atoi(sCmd.next());
+        int kickPower = atoi(sCmd.next());
+        moveMotor(DRIBBLER, dribbler);
+        moveMotor(KICKERS, kickPower);
+        moveMotor(KICKERS2, -kickPower);
 }
 
 
 void kicker(){
-    int type = atoi(sCmd.next());
-    if(type == 0) {
-      motorStop(KICKERS);
-    } else if (type == 1) {
-      Serial.print("Starting From: ");
-      Serial.println(positions[0] % 40);
-      motorForward(KICKERS, 100);
-      kickerStatus = 1;
-    } else {
-      motorBackward(KICKERS, 100);
-      kickerStatus = -1;
-    }
-}
-
-void completeHalt(){
-    motorAllStop();
-    motorControl(FRONTLEFT, 0);
-    motorControl(BACK, 0);
-    motorControl(FRONTRIGHT, 0);
-    motorControl(DRIBLER, 0);
-}
-
-void pingMethod(){
-    Serial.println("pang");
-}
-
-void setup(){
-    Wire.begin();
-    sCmd.addCommand("f", dontMove);
-    sCmd.addCommand("h", completeHalt);
-    //sCmd.addCommand("motor", moveMotor);
-    sCmd.addCommand("r", rationalMotors);
-    sCmd.addCommand("dk", dribblerKick);
-    sCmd.addCommand("ping", pingMethod);
-    sCmd.addCommand("kick", kicker);
-    sCmd.addCommand("mux", muxTest);
-    SDPsetup();
+        int type = atoi(sCmd.next());
+        if(type == 0) {
+                motorStop(KICKERS);
+        } else if (type == 1) {
+                Serial.print("Starting From: ");
+                Serial.println(positions[0] % 40);
+                motorForward(KICKERS, 100);
+                kickerStatus = 1;
+        } else {
+                motorBackward(KICKERS, 100);
+                kickerStatus = -1;
+        }
 }
 
 
+
+
+//  ====================================
+//      MISC
+//  ====================================
 void printMotorPositions() {
-    Serial.print("Motor positions: ");
-    for (int i = 0; i < ROTARY_COUNT; i++) {
-            Serial.print(positions[i]);
-            Serial.print(' ');
-    }
-    Serial.println();
-    delay(PRINT_DELAY); // Delay to avoid flooding serial out
+        Serial.print("Motor positions: ");
+        for (int i = 0; i < ROTARY_COUNT; i++) {
+                Serial.print(positions[i]);
+                Serial.print(' ');
+        }
+        Serial.println();
+        delay(PRINT_DELAY); // Delay to avoid flooding serial out
+}
+
+
+void muxTest(){
+        int motor = atoi(sCmd.next());
+        int dir  = atoi(sCmd.next());
+        int pow  = atoi(sCmd.next());
+        Wire.beginTransmission(OPADDR);
+        Wire.write(motor);
+        Wire.write(dir);
+        Serial.println(Wire.endTransmission());
+        Wire.beginTransmission(OPADDR);
+        Wire.write(motor+1);
+        Wire.write(pow);
+        Serial.println(Wire.endTransmission());
+        delay(2000);
+        Wire.beginTransmission(OPADDR);
+        Wire.write(motor);
+        Wire.write(0);
+        Serial.println(Wire.endTransmission());
+}
+
+
+
+// ======================================================
+// we did not use this! this is part of the original code in FRED.
+// See moveMotor instead!
+void motorControl(int motor, int power){
+        if(power == 0) {
+                Wire.beginTransmission(OPADDR);
+                Wire.write(motor);
+                Wire.write(0);
+                Wire.endTransmission();
+        } else if(power > 0) {
+                Wire.beginTransmission(OPADDR);
+                Wire.write(motor);
+                Wire.write(1);
+                Wire.endTransmission();
+                Wire.beginTransmission(OPADDR);
+                Wire.write(motor + 1);
+                Wire.write(power);
+                Wire.endTransmission();
+        } else {
+                Wire.beginTransmission(OPADDR);
+                Wire.write(motor);
+                Wire.write(2);
+                Wire.endTransmission();
+                Wire.beginTransmission(OPADDR);
+                Wire.write(motor + 1);
+                Wire.write(-power); // note change of sign
+                Wire.endTransmission();
+        }
 }
