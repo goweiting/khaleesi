96929aa464021fe2624dc83ae3813442039b6ac0
Radoslav <radmrkv@gmail.com>
2017-02-13 01:23:55 +0000

    and a fix for that

--- sdpJava/src/strategy/actions/other/HoldPosition.java
+++ sdpJava/src/strategy/actions/other/HoldPosition.java
@@ -2,22 +2,30 @@ package strategy.actions.other;
 
 import strategy.Strategy;
 import strategy.actions.ActionBase;
+import strategy.points.DynamicPoint;
+import strategy.robots.Khaleesi;
 
 /**
  * Created by Simon Rovder
  * <p>
  * When the robot fails, it shall Contemplate...
  */
-public class Contemplating extends ActionBase {
-    public Contemplating() {
-        this.rawDescription = "Contemplating...";
+public class HoldPosition extends ActionBase {
+    private DynamicPoint targetPosition;
+
+    public HoldPosition(DynamicPoint target) {
+        targetPosition = target;
+        this.rawDescription = "Holding position [X " + targetPosition.getX() + ";" +
+                              "Y " + targetPosition.getY() + "]";
     }
 
     @Override
     public void onStart() {
-        // Literally stop doing anything. Just stop. STAAAHP.
-        Strategy.currentRobotBase.setControllersActive(false);
-        Strategy.currentRobotBase.port.stop();
+        // Set target position as a destination
+        Khaleesi us = (Khaleesi)Strategy.currentRobotBase;
+        us.MOTION_CONTROLLER.setDestination(targetPosition);
+        us.MOTION_CONTROLLER.setHeading(targetPosition);
+        us.MOTION_CONTROLLER.setTolerance(-1);
     }
 
     @Override
