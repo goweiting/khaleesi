51e096851f301c05781dcb2f2da6d13928ad9599
goweiting <gwt.543@gmail.com>
2017-03-18 21:33:58 +0000

    Added vision settings and start script from previous repo

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd java
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$DIR/libs/"
+export LD_LIBRARY_PATH=$DIR
+java -cp out/production/sdpJava/:libs/jssc.jar:libs/v4l4j.jar  strategy.Strategy
