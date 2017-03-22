32560aa28fa6fac88e5969fd896b179120f8fcac
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-03-22 12:47:02 +0000

    singleVision with defensive behaviour!

---
+++ runOut_singleVision.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+cd java 
+pwd 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+DIR="$DIR/libs/" 
+export LD_LIBRARY_PATH=$DIR 
+java -cp ../out_singleVision/production/khaleesi:libs/jssc.jar:libs/v4l4j.jar strategy.Strategy 
