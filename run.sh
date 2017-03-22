d706c6269af80fd6cc5d95a61f96064d09b48f8c
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-03-22 11:24:10 +0000

    new run script for changes in out folder

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 cd java
+pwd
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$DIR/libs/"
 export LD_LIBRARY_PATH=$DIR
-java -cp out/production/sdpJava/:libs/jssc.jar:libs/v4l4j.jar  strategy.Strategy
+java -cp ../out/production/khaleesi:libs/jssc.jar:libs/v4l4j.jar strategy.Strategy
