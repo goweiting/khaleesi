28c12d473411c9fcde66ad0b479d8f064731dc4c
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-04-06 13:47:16 +0100

    added behaviour for flapping into software

---
+++ java/src/communication/ports/interfaces/AngryBirdPort.java
@@ -0,0 +1,12 @@
+package communication.ports.interfaces;
+
+/**
+ * Created by GWT on 06/04/17.
+ */
+public interface AngryBirdPort {
+    // This interface implements the flapping actions
+    public void toggle();
+    public void flap();
+
+
+}
